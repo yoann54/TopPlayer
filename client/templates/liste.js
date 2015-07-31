f11e48325baf80ecf29bf33c6d5c1ebcf29f25a0
yoann54 <yoann.p@laposte.net>
2015-07-31 17:09:55 +0200

    fin du chapitre 7 : Plus loin avec MongoDb

--- client/templates/liste.js
+++ client/templates/liste.js
@@ -1,18 +1,26 @@
 Template.liste.helpers({
     'joueur': function () {
-        return joueursListe.find();
+        return joueursListe.find({}, {sort: {score:-1, name:1}});
     },
     'classClick': function () {
         var joueurId = this._id;
         var joueurChoisi = Session.get('joueurChoisi');
-        if (joueurChoisi == joueurId) {
-            return 'select';   
+        if (joueurChoisi === joueurId) {
+            return 'select';
         }
     }
-}); 
+});
 Template.liste.events({
     'click td.choisi': function () {
         var joueurId = this._id;
         Session.set('joueurChoisi', joueurId);
+    },
+    'click .increment': function () {
+        var joueurChoisi = Session.get('joueurChoisi');
+        joueursListe.update(joueurChoisi, {$inc: {score: 1}});
+    },
+    'click .decrement':function () {
+        var joueurChoisi = Session.get('joueurChoisi');
+        joueursListe.update(joueurChoisi, {$inc: {score:-1} });
     }
 });
