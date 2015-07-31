0b7f6b50c5f5d1dff164d2b680d53a35531cb84e
yoann54 <yoann.p@laposte.net>
2015-07-31 15:22:30 +0200

    fin du chapitre 6 : Packages, événements et sessions

--- client/templates/liste.js
+++ client/templates/liste.js
@@ -1,5 +1,18 @@
 Template.liste.helpers({
     'joueur': function () {
         return joueursListe.find();
+    },
+    'classClick': function () {
+        var joueurId = this._id;
+        var joueurChoisi = Session.get('joueurChoisi');
+        if (joueurChoisi == joueurId) {
+            return 'select';   
+        }
+    }
+}); 
+Template.liste.events({
+    'click td.choisi': function () {
+        var joueurId = this._id;
+        Session.set('joueurChoisi', joueurId);
     }
 });
