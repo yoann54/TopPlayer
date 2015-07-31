9b607dfd7d3d20ece5cc59881e0e67fe05a043ba
yoann54 <yoann.p@laposte.net>
2015-07-31 14:39:50 +0200

    fin du chapitre 3 : Les templates

---
+++ client/templates/liste.js
@@ -0,0 +1,8 @@
+Template.liste.helpers({
+    'merci': function(){
+        console.log('merci mon helper !!!');
+    },
+    'title' : function(){
+    return "un petit helper de plus !!!"
+    }
+});
