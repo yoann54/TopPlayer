16c93fbf0e0b58e45e303bfd45db10be57ae4140
yoann54 <yoann.p@laposte.net>
2015-07-31 15:00:59 +0200

    fin du chapitre 5 : Plus loin avec les templates

--- client/templates/liste.js
+++ client/templates/liste.js
@@ -1,8 +1,5 @@
 Template.liste.helpers({
-    'merci': function(){
-        console.log('merci mon helper !!!');
-    },
-    'title' : function(){
-    return "un petit helper de plus !!!"
+    'joueur': function () {
+        return joueursListe.find();
     }
 });
