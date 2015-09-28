f77f74418483595a98fc7325eb8ae62f5134523a
Yoann Piconcely <yoann.p@laposte.net>
2015-09-28 11:10:12 +0200

    Update ajoutjoueur.js

--- client/templates/ajoutjoueur.js
+++ client/templates/ajoutjoueur.js
@@ -1,6 +1,6 @@
 Template.ajoutJoueur.events({
     'submit form': function (event) {
-        event.preventDefault();
+        event.preventDefault(event);
         var joueurName = event.target.nomJoueur.value;
         Meteor.call('ajoutJoueurMethode', joueurName);
     }
